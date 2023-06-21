5128ba151c87fed5e1dacd2927b6ddf7d9804ca1
goku <wkurts247@gmail.com>
2023-06-21 20:00:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+const random = Math.floor(Math.random()*10+1);
+console.log(random);
+let num3 = 10;
+let num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+const max = Math.max(20, 12, 14, 9, 11);
+console.log(max);
